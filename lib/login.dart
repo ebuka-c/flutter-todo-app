d25ac7a8d3fc1a47439af29b81e4449c2824a1d1
ebuka-c <chiebuka873@gmail.com>
2023-09-21 21:43:13 +0100

    start todo app

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _Login_ScreenState();
+}
+
+class _Login_ScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
